72d6407491dce5df21bd95138f09741170a5cf7b
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-01-09 10:46:18 +0700

    class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import './style.css'
+// import './style.css'
+import { Header, Content, Footer } from "./func-components";
 
 function App() {
-  // let r = 'React'
-  // let rn = 'React Native'
-  // const el = (
-  //   <div>
-  //     Create Web & Mobile Apps <br/>
-  //     with {r} & {rn}
-  //   </div>
-  // )
-
+ 
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className="title">
-        React & React Native
-      </div>
-    </>
+    [<Header/>, <Content/>, <Footer/>]
   )
 }
 
