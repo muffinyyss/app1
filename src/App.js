217b8749c41905c8a99b283e6732105031451223
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-01-16 10:02:14 +0700

    EventClass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { Header, Content, Footer } from "./func-components";
-import Calendar from "./class-components";
+// import { Header, Content, Footer } from "./func-components";
+// import Calendar from "./class-components";
+import Button from "./event-class";
 
 function App() {
  
-  return (
-    <>
-      <Header/>
-      <p><center> <Calendar/> </center></p>
-      <Content/>
-      <Footer/>
-    </>
-    
-  )
+  return <Button/>
 }
 
 export default App;
