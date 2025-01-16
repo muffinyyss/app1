529d184ec6735bc9255040ffd4e77da6ef51a10a
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-01-16 11:31:25 +0700

    event-data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-// import { Header, Content, Footer } from "./func-components";
-// import Calendar from "./class-components";
-import Button from "./event-class";
-import { Calculator2 } from "./calculator";
+import { EventData2 } from "./event-data";
 
-function App() {
- 
-  return <Calculator2/>
+export default function App(){
+  return <EventData2/>
 }
-
-export default App;
