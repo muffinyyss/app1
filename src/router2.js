9ebc7b6811bfb0995a45bd49787b2acf0f061d92
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-02-13 11:11:47 +0700

    router3.js

---
+++ src/router2.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, NavLink } from "react-router-dom";
+import "./navlink.css";
+
+export default function Router2() {
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active_menu" : "menu")}
+          style={({ isActive }) => {    //กำหนด style เพิ่มเติมนอกเหนือจาก className
+            return {
+              fontWeight: isActive ? "bold" : ""
+            };
+          }}
+        >
+          Home
+        </NavLink> -&nbsp;
+
+        <NavLink to='/products'
+         className={({isActive}) => isActive ? "active_menu" : "menu"}
+        >
+          Products
+        </NavLink> -&nbsp;
+
+        <NavLink to='/member'
+         className={({isActive}) => isActive ? "active_menu" : "menu"}
+        >
+          Member
+        </NavLink> -&nbsp;
+
+        <NavLink to='/contact'
+         className={({isActive}) => isActive ? "active_menu" : "menu"}
+        >
+          Contact Us
+        </NavLink>
+
+      </nav>
+    </BrowserRouter>
+  );
+}
